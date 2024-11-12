70576c221cff8bab168bfabedb5d79f161b5a9e8
Vince <freestyle2.0313@gmail.com>
2024-11-12 23:41:57 +0800

    example feature

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ const Home = () => {
                                         <a
                                             href="https://laravel.com/docs"
                                             className="underline text-gray-900 dark:text-white">
-                                            Documentation
+                                            TANGINA MO ZEK
                                         </a>
                                     </div>
                                 </div>
