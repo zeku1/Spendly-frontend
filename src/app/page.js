b7218c971bb71e8dd18d7b35ac226ec2fb573e4a
Vince <freestyle2.0313@gmail.com>
2024-11-20 05:39:45 +0800

    fix axious

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ const Home = () => {
                                         <a
                                             href="https://laravel.com/docs"
                                             className="underline text-gray-900 dark:text-white">
-                                            TANGINA MO ZEK at MARC at ZED
+                                                test
                                         </a>
                                     </div>
                                 </div>
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,12 +1,31 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL, // Note: changed from BACKEND_URL
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'Accept': 'application/json',
     },
     withCredentials: true,
-    withXSRFToken: true
 })
+
+// Add interceptors for debugging
+axios.interceptors.request.use(config => {
+    console.log('Request URL:', config.url);
+    console.log('Request Method:', config.method);
+    console.log('Base URL:', config.baseURL);
+    return config;
+}, error => {
+    console.error('Request Error:', error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        console.error('Full Error Response:', error.response);
+        return Promise.reject(error);
+    }
+);
 
 export default axios
